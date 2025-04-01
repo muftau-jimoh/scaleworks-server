9a4848990b3624928cdce305742a2eae8f80419e
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2025-04-01 07:49:01 +0100

    fixed the authMiddleware bug

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,26 +1,27 @@
-const supabase = require('../config/supabaseClient');
-const { getUserByAuthId } = require('../utils/getUserByAuthId');
+const supabase = require("../config/supabaseClient");
+const { getUserByAuthId } = require("../utils/getUserByAuthId");
 
 exports.isAuthenticatedUser = async (req, res, next) => {
-    const authHeader = req.headers.authorization;
-    
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        return res.status(401).json({ error: "Unauthorized: No token provided" });
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Unauthorized: No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const { data, error } = await supabase.auth.getUser(token);
+
+    if (error || !data.user) {
+      return res.status(401).json({ error: "Unauthorized: Invalid token" });
     }
 
-    const token = authHeader.split(" ")[1];
+    req.user = await getUserByAuthId(data?.user?.id);
 
-    try {
-        const { data, error } = await supabase.auth.getUser(token);
-
-        if (error || !data.user) {
-            return res.status(401).json({ error: "Unauthorized: Invalid token" });
-        }
-
-        req.user = data.user; // Attach user data to request
-        next();
-    } catch (err) {
-        // console.error('Authentication error:', err);
-        return res.status(500).json({ error: "Internal server error" });
-    }
+    next();
+  } catch (err) {
+    // console.error('Authentication error:', err);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 };
